7a78f9186d426d503946d7fe07b0d370d1df00b4
snvn1511 <spxedu@gmail.com>
2023-02-02 16:06:42 +0700

    thêm chức năng post

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React , {useState}from 'react'
+import React, { useState } from 'react'
 import { Button, FlatList, View, Text } from 'react-native';
 
 var api_url = "https://63db6922a3ac95cec5a10e24.mockapi.io/demo-api/sanpham";
@@ -22,20 +22,50 @@ export default function App() {
       });
   }
 
+  // hàm thêm dữ liệu
+  const postAddSP = () => {
+    // 1. chuẩn bị dữ liệu 
+    let objSP = {
+        name:"Xe máy 11111",
+        price: 333333 // có thể gán dữ liệu từ state của TextInput
+    }
+    //2. fetch
+
+    fetch( api_url , {
+      method: 'POST',// POST: Thêm, PUT: Sửa, DELETE: xóa
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(  objSP   ),
+    })
+    .then ( (res)=>{
+          console.log(res);
+          if(res.status==201)
+            alert("Thêm thành công")
+    })
+    .catch( (err)=>{ console.log( err );})
+    ;
+  }
+
+
+
+
   return (
     <View style={{ margin: 30 }}>
       <Button title='Load list' onPress={getListSP} />
       {/* Hiển thị danh sách */}
       <FlatList
-      data={dssp}
-      keyExtractor={ (item)=>{ return item.id } }
-      renderItem = { ({item})=> (
-          <View style={{backgroundColor:"yellow", margin:5}}>
-          <Text> {item.name} -- {item.price}  </Text>
+        data={dssp}
+        keyExtractor={(item) => { return item.id }}
+        renderItem={({ item }) => (
+          <View style={{ backgroundColor: "yellow", margin: 5 }}>
+            <Text> {item.name} -- {item.price}  </Text>
           </View>
-      ) }
+        )}
       />
 
+          <Button title='Add new ' onPress={postAddSP } />
 
     </View>
   );
